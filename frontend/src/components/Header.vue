67d181be38a735d183effb377c7d2f41aa75d977
oleg <zustzero123@gmail.com>
2025-03-07 01:21:10 +0300

    _

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header">
+    <button class="goBack">< Назад</button>
+    <img src="../assets/logo.png" alt="#" class="logo" />
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.header {
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
